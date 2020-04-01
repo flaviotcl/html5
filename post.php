fbfd355984f6bc55c06e45c5624f9131193cd7b9
Flavio tavares <tavares.pmsg@gmail.com>
2020-04-01 13:35:36 -0300

    Attribute Form + Imprimindo [xxxxx]

---
+++ post.php
@@ -0,0 +1,10 @@
+<?php 
+//echo phpinfo();
+echo $_GET["fname"]."&nbsp&nbsp;&nbsp;";
+echo $_GET["lname"]."&nbsp;&nbsp;&nbsp;";
+echo $_GET["email"]."&nbsp;&nbsp;&nbsp;";
+echo $_GET["color"];
+?>
+
+
+
